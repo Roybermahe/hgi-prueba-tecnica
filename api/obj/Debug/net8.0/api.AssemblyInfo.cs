d1b763aa53c5083ffabe7cbc459dbb556f5ee11d
Roybermahe <roybermanjarrez@gmail.com>
2024-09-26 16:52:47 -0500

    corrigiendo ejecución

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c022ffe8fe9b4c70ffc75af298b8f125645a9ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001f615a0365f4e9917f976dca4f2bf2a1debdfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
